5d7959441782ada429ed029f5ac0718cd6e92e8e
Sheikh Salman Hossain <66014790+sheikhsalmanhossain@users.noreply.github.com>
2026-01-27 03:15:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "123, This is salman. I am a DevOps engineer"}
+    return {"message": "123, I'm salman. I am a DevOps engineer"}
